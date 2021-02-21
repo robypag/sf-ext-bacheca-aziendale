a40cee10f60a4473deb7415ad4345f438111199d
Roberto Pagni <roberto.pagni@sap.com>
2021-02-22 00:51:17 +0100

    [1.0.2] UI Boilerplates

---
+++ app/employee/webapp/controller/Home.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/core/Fragment",
+    ],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.core.Fragment} Fragment
+     */
+    function (Controller, JSONModel, Fragment) {
+        "use strict";
+
+        return Controller.extend("itg.sf.bacheca.controller.Home", {
+            onInit: function () {},
+        });
+    }
+);
